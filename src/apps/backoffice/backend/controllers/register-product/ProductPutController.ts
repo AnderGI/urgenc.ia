b5c3974f89cef36d4b9cac1f118e30e4987e0a65
AnderGI <agibarguen@gmail.com>
2025-07-26 14:53:04 +0200

    feat(register-product): handle put data stream on controller to parse its json

--- src/apps/backoffice/backend/controllers/register-product/ProductPutController.ts
+++ src/apps/backoffice/backend/controllers/register-product/ProductPutController.ts
@@ -3,6 +3,17 @@ import status from "http-status";
 
 export default class ProductPutController {
   public run(req: Request, res: Response): void {
+    const data:Buffer[] = [] 
+    req.on('data', (_:Buffer) => {
+      data.push(_)
+    })
+
+    req.on('end', () => {
+        const body = Buffer.concat(data).toString();
+        const jsonBody = JSON.parse(body);
+        console.log(jsonBody);
+    })
+
     res.status(status.ACCEPTED).send();
   }
 }
